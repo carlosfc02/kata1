f27b3d2ac58403f4d31542e555f3e4b9e3bd9a22
carlosfc02 <carlosfalconcastellano@gmail.com>
2024-01-19 18:40:29 +0000

    Class Main done

--- src/software/ulpgc/kata1/Main.java
+++ src/software/ulpgc/kata1/Main.java
@@ -1,4 +1,11 @@
 package software.ulpgc.kata1;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Carlos", LocalDate.of(2002,9,27));
+        System.out.println(person);
+        System.out.println("Age = " + person.getAge());
+    }
 }
