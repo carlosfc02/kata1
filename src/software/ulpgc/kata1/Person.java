235290c1a66cdbcd60ea5e1a11725e8bc8850889
carlosfc02 <carlosfalconcastellano@gmail.com>
2024-01-19 18:38:22 +0000

    Class Person with constructor, getters, toString and method getAge(). Class Person done

--- src/software/ulpgc/kata1/Person.java
+++ src/software/ulpgc/kata1/Person.java
@@ -1,4 +1,37 @@
 package software.ulpgc.kata1;
 
+import java.time.LocalDate;
+
 public class Person {
+    private final String name;
+    private final LocalDate birthday;
+
+    public Person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
+
+    public int getAge(){
+        return toYear(LocalDate.now().toEpochDay() - this.birthday.toEpochDay());
+    }
+
+    private int toYear(long day) {
+        return (int) (day/365.25);
+    }
 }
